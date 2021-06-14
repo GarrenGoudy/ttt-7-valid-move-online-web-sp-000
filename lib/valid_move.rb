025976849e3ab25b5757384cf1e531830b8e1665
Garren Goudy <garren.goudy@yahoo.com>
2021-06-14 17:35:19 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,7 +2,7 @@
 
 def valid_move?(board, index)
 	spot = position.to_i - 1
-	if !position_taken?(board, spot) && spot.between?(0,8)
+	if position_taken?(board, spot) && spot.between?(0,8)
 		true
 
 	else
